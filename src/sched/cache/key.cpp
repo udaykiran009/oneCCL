5bfee495a8f74348fe4b53cd620c854658654541
Kochanova, Ksenya <ksenya.kochanova@intel.com>
2020-04-24 20:38:03 +0300

    Remove allgatherv recv_counts from full cache key (#390)

--- src/sched/cache/key.cpp
+++ src/sched/cache/key.cpp
@@ -30,7 +30,6 @@ void ccl_sched_key::set(const ccl_coll_param& param,
     switch (f.ctype)
     {
         case ccl_coll_allgatherv:
-            f.buf1 = (void*)param.recv_counts;
             f.count1 = param.send_count;
             break;
         case ccl_coll_allreduce:
@@ -83,8 +82,7 @@ bool ccl_sched_key::check(const ccl_coll_param& param,
     switch (f.ctype)
     {
         case ccl_coll_allgatherv:
-            result &= (param.recv_counts == f.buf1 &&
-                       param.send_count == f.count1);
+            result &= (param.send_count == f.count1);
             break;
         case ccl_coll_allreduce:
             result &= (param.count == f.count1 &&
