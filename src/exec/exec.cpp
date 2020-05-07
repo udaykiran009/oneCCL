28fece8c6b5ea2b23a3105c86b7fc485be355aa6
Shiryaev, Mikhail <mikhail.shiryaev@intel.com>
2020-05-07 04:55:57 +0300

    Add explicit round-robin between workers (#396)

--- src/exec/exec.cpp
+++ src/exec/exec.cpp
@@ -4,6 +4,17 @@
 #include "unordered_coll/unordered_coll.hpp"
 #include "sched/extra_sched.hpp"
 
+size_t ccl_executor::get_worker_idx_by_sched_id(ccl_sched* sched)
+{
+    return sched->sched_id % workers.size();
+}
+
+size_t ccl_executor::get_worker_idx_round_robin(ccl_sched* sched)
+{
+    ++rr_worker_idx %= workers.size();
+    return rr_worker_idx;
+}
+
 size_t ccl_executor::calculate_atl_ep_count(size_t worker_count)
 {
     size_t ep_count = worker_count;
@@ -26,6 +37,10 @@ std::unique_ptr<ccl_sched_queue> ccl_executor::create_sched_queue(size_t idx, si
 
 ccl_executor::ccl_executor(const char* main_addr)
 {
+    get_worker_idx_fn = (env_data.enable_fusion || env_data.enable_unordered_coll) ?
+        &ccl_executor::get_worker_idx_by_sched_id :
+        &ccl_executor::get_worker_idx_round_robin;
+
     auto worker_count = env_data.worker_count;
     workers.reserve(worker_count);
     auto ep_count = calculate_atl_ep_count(worker_count);
@@ -248,7 +263,7 @@ void ccl_executor::start(ccl_master_sched* sched)
     size_t worker_idx;
     for (size_t idx = 0; idx < sched->partial_scheds.size(); idx++)
     {
-        worker_idx = sched->partial_scheds[idx]->sched_id % workers.size();
+        worker_idx = (this->*get_worker_idx_fn)(sched->partial_scheds[idx].get());
         workers[worker_idx]->add(sched->partial_scheds[idx].get());
     }
 }
