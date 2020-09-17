423de3617b24ed99adfeda77157ee3de3dadcddc
Anenkov, Aleksandr <aleksandr.anenkov@intel.com>
2020-09-17 12:47:30 +0300

    Hid internal implementation of request (#576)

---
+++ src/common/request/request_impl.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace ccl {
+
+class event;
+
+class request_impl {
+public:
+    virtual void wait() = 0;
+    virtual bool test() = 0;
+    virtual bool cancel() = 0;
+    virtual event& get_event() = 0;
+    virtual ~request_impl() = default;
+};
+
+}
