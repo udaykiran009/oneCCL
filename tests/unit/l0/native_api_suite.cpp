3b859aa9ddd036afc7b1b481ccaa5cb277dc2f0a
Ivanov, Sergey <sergey.ivanov@intel.com>
2020-10-09 14:11:05 +0300

    Added platform info tests (#656)
    Added platform info UT

--- tests/unit/l0/native_api_suite.cpp
+++ tests/unit/l0/native_api_suite.cpp
@@ -1,6 +1,7 @@
 #include "base.hpp"
 #include "fixture.hpp"
 #include "native_api/subdevice_test.hpp"
+#include "native_api/platform_test.hpp"
 
 int main(int ac, char* av[]) {
     set_test_device_indices(getenv("L0_CLUSTER_AFFINITY_MASK"));
