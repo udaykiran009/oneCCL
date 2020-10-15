332f48327bc533b1801526a59ae4f87027d52deb
sivanov <sergey.ivanov@intel.com>
2020-10-15 13:37:51 +0300

    Fix export.sh for single device

--- tests/unit/l0/base.hpp
+++ tests/unit/l0/base.hpp
@@ -4,6 +4,7 @@ void set_test_device_indices(const char *indices_csv = nullptr);
 #ifndef STANDALONE_UT
 #include <gtest/gtest.h>
 #else // STANDALONE_UT
+#include <tuple>
 namespace testing {
 class Test {
 public:
@@ -15,7 +16,9 @@ public:
 };
 
 template<class ...T>
-struct Types {};
+struct Types {
+    using list_t = std::tuple<T...>;
+};
 } // namespace testing
 
 #define TEST_F(fixture, test_name)                      \
@@ -29,22 +32,25 @@ struct Types {};
     };                                                  \
     void Test_##test_name::invoke()
 
-#define TYPED_TEST_CASE(fixture, test_name)
+#define TYPED_TEST_CASE(fixture, types)                                 \
+using type_list_##fixture = typename types::list_t;                               \
+using first_type_##fixture = typename std::tuple_element<0, type_list_##fixture>::type;
 
+using default_data_type = float;
+
+// TODO Only first type from TYPED_TEST_CASE is invoking
 #define TYPED_TEST(fixture, test_name)                  \
-    struct TypedTest_##test_name : public fixture {          \
-        struct TypedParam                               \
-        {                                               \
-            using first_type = uint8_t;                 \
-            using second_type = first_type;             \
-        };                                              \
+    struct TypedTest_##test_name : public fixture<first_type_##fixture> {          \
+                                                        \
+        template<class T>                               \
         void invoke();                                  \
         void start() {                                  \
             SetUp();                                    \
-            invoke<TypedParam>();                       \
+            invoke<first_type_##fixture>();             \
             TearDown();                                 \
         }                                               \
     };                                                  \
-    void TypedTest_##test_name::invoke<TypedParam>()
+    template<class TypeParam>                           \
+    void TypedTest_##test_name::invoke()
 
 #endif // STANDALONE_UT
