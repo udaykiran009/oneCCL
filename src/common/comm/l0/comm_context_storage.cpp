70e4b4fa9834aac8c5c8d078f2a86944b036dade
Stepykin, Ilya <ilya.stepykin@intel.com>
2020-11-04 08:27:33 +0300

    Indicate that we don't support multiple ranks per process (#763)
    This adds a check for a case when we have multiple ranks
    per process, but each of them has local size = 1.
    The check is done after the sync once the total number
    of ranks in the process is available. And if the number
    is > 1, throw unimplemented exception, similar to how
    it's done for a general case(local size > 1)
    
    Signed-off-by: Ilya Stepykin <ilya.stepykin@intel.com>

--- src/common/comm/l0/comm_context_storage.cpp
+++ src/common/comm/l0/comm_context_storage.cpp
@@ -23,6 +23,16 @@ group_context::comm_group_t group_context::group_by_kvs(
         std::shared_ptr<atl_wrapper>(new atl_wrapper(cluster_device_group_size,
                                                     local_thread_device_group_ranks,
                                                     kvs_wrap));
+
+    /* Indicate that multiple devices are not supported, don't throw anything if kernel_path env variable
+     * is set to enable our testing with partial functionality.
+     * Most of the cases are handled in communicator_impl_details.hpp, but here we check the case
+     * when we have multiple threads and each of them has 1 device. And we don't know the total number
+     * of ranks in the process until we sync them above */
+    if (atl->get_ranks_per_process() > 1 && ccl::global_data::env().kernel_path.empty()) {
+        throw ccl::unimplemented("API", "create_communicators", "for multiple devices");
+    }
+
     LOG_INFO("Thread released by barrier");
     LOG_INFO("Cluster_device_group size: ", cluster_device_group_size,
              "\nThread device group ranks size: ", local_thread_device_group_ranks.size());
