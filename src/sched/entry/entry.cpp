27e788359aca3120b6d29bfab7e60fc7f0ba4916
Gusev, Nikita <nikita.gusev@intel.com>
2021-05-04 21:21:20 +0300

    Fix l0_entry case: skip flow control for gpu case & fix incorrect I_MPI_ROOT (#1085)
    * Fix l0_entry case: skip flow control for gpu case
    
    * cmake: fix incorrect I_MPI_ROOT

--- src/sched/entry/entry.cpp
+++ src/sched/entry/entry.cpp
@@ -6,13 +6,23 @@ void sched_entry::do_progress() {
     if (is_completed())
         return;
 
+    // TODO: fix this tempropary workaround
+    // For l0 entry take_credit & return_credit isn't needed
+    // That's why we'd skip it
+    bool is_l0_entry = false;
+    const char* name_entry = this->name();
+
+    //  in case if entry is empty name or its  length = 1
+    if (strlen(name_entry) >= 2)
+        is_l0_entry = name_entry[0] == 'L' && name_entry[1] == '0';
+
     if (status < ccl_sched_entry_status_started) {
         CCL_ASSERT(
             status == ccl_sched_entry_status_not_started || status == ccl_sched_entry_status_again,
             "bad status ",
             status);
 
-        if (sched->flow_control.take_credit()) {
+        if (is_l0_entry || sched->flow_control.take_credit()) {
             start();
             CCL_ASSERT(status >= ccl_sched_entry_status_again, "bad status ", status);
         }
@@ -26,7 +36,7 @@ void sched_entry::do_progress() {
         CCL_ASSERT(status >= ccl_sched_entry_status_started, "bad status ", status);
     }
 
-    if (status == ccl_sched_entry_status_complete) {
+    if (status == ccl_sched_entry_status_complete && !is_l0_entry) {
         sched->flow_control.return_credit();
     }
 
