6f669736fe714ffb80ac623d654b19ca041249e3
Anenkov, Aleksandr <aleksandr.anenkov@intel.com>
2021-06-03 12:59:40 +0300

    Fix jenkins utils path and warnings (#1048)

--- scripts/abi/abi-compare.sh
+++ scripts/abi/abi-compare.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -o pipefail
 
-readonly ARGS="$@"
-readonly ME=$(basename $0)
+readonly ARGS=( "$@" )
+readonly BASENAME=$(basename $0)
 
-readonly EXIT_FAILURE=-1
+readonly EXIT_FAILURE=1
 
 print_help()
 {
-    echo "Usage: $ME --name NAME --old PATH --new PATH --report PATH"
+    echo "Usage: ${BASENAME} --name NAME --old PATH --new PATH --report PATH"
     echo "  --name NAME             any name of the library"
     echo "  --old PATH              path to the old 'build' dir"
     echo "  --new PATH              path to the new 'build' dir"
@@ -26,30 +26,30 @@ print_help()
     echo
     echo "Usage examples:"
     echo "  1) Comparison of two builds:"
-    echo "     $ME --name libccl_cpu --old ./mlsl_old/build --new ./mlsl_new/build --report-dir ./report"
+    echo "     ${BASENAME} --name libccl_cpu --old ./mlsl_old/build --new ./mlsl_new/build --report-dir ./report"
     echo "     or"
-    echo "     $ME --name libccl_gpu --old ./mlsl_old/build_gpu --new ./mlsl_new/build_gpu --report-dir ./report"
+    echo "     ${BASENAME} --name libccl_gpu --old ./mlsl_old/build_gpu --new ./mlsl_new/build_gpu --report-dir ./report"
     echo
     echo "  2) Comparison of two builds after the post build step:"
-    echo "     $ME --name libccl_cpu --old ./mlsl_old/build --new ./mlsl_new/build --report-dir ./report --build-type cpu_icc"
+    echo "     ${BASENAME} --name libccl_cpu --old ./mlsl_old/build --new ./mlsl_new/build --report-dir ./report --build-type cpu_icc"
     echo "     or"
-    echo "     $ME --name libccl_gpu --old ./mlsl_old/build --new ./mlsl_new/build --report-dir ./report --build-type cpu_gpu_dpcpp"
+    echo "     ${BASENAME} --name libccl_gpu --old ./mlsl_old/build --new ./mlsl_new/build --report-dir ./report --build-type cpu_gpu_dpcpp"
 }
 
 parse_arguments()
 {
     while [ $# -ne 0 ]
     do
-        case $1 in
+        case ${1} in
             "-h" | "--help" | "-help")                  print_help ; exit 0 ;;
-            "--name" | "-name")                         LIBRARY_NAME=$2 ; shift ;;
-            "--old" | "-old")                           OLD_BUILD=$(realpath $2) ; shift ;;
-            "--new" | "-new")                           NEW_BUILD=$(realpath $2) ; shift ;;
-            "--report-dir" | "-report-dir")             REPORT_DIR=$(realpath $2) ; shift ;;
-            "--dump-dir" | "-dump-dir")                 DUMP_DIR=$(realpath $2) ; shift ;;
-            "--build-type" | "-build-type")             BUILD_TYPE=$2 ; shift ;;
+            "--name" | "-name")                         LIBRARY_NAME=${2} ; shift ;;
+            "--old" | "-old")                           OLD_BUILD="$(realpath ${2})" ; shift ;;
+            "--new" | "-new")                           NEW_BUILD="$(realpath ${2})" ; shift ;;
+            "--report-dir" | "-report-dir")             REPORT_DIR="$(realpath ${2})" ; shift ;;
+            "--dump-dir" | "-dump-dir")                 DUMP_DIR="$(realpath ${2})" ; shift ;;
+            "--build-type" | "-build-type")             BUILD_TYPE=${2} ; shift ;;
             *)
-            echo "$ME: ERROR: unknown option ($1)"
+            echo "${BASENAME}: ERROR: unknown option (${1})"
             print_help
             exit ${EXIT_FAILURE}
             ;;
@@ -75,7 +75,7 @@ parse_arguments()
 
 check_command()
 {
-    local cmd=$1
+    local cmd=${1}
     command -v ${cmd} >/dev/null 2>&1 || { echo >&2 "Error: ${cmd} wasn't found. Aborting."; exit ${EXIT_FAILURE}; }
 }
 
@@ -84,7 +84,7 @@ set_env()
     source /p/pdsd/scratch/Uploads/CCL_oneAPI/compiler/last/compiler/latest/env/vars.sh intel64
     check_command clang++
 
-    UTILS_DIR=/nfs/inn/disks/nn-ssg_tcar_mpi_2Tb_unix/jenkins/artefacts/jenkins-utils/CCL/abi
+    UTILS_DIR=/nfs/inn/disks/nn-ssg_tcar_mpi_2Tb_unix/Uploads/CCL/jenkins-utils/abi
     export PATH=${UTILS_DIR}/abi-compliance-checker/bin:$PATH
     check_command abi-compliance-checker
 
@@ -107,8 +107,8 @@ declare -i dump_version=0
 
 generate_dump()
 {
-    local build_dir=$1
-    local output=$2
+    local build_dir=${1}
+    local output=${2}
     ((dump_version++))
 
     local so_name="libccl.so"
@@ -131,17 +131,17 @@ generate_dump()
 
 print_abi_breakages()
 {
-    local name=$1
+    local name=${1}
     local abi_affected="${REPORT_DIR}/${name}/abi_affected.txt"
     local src_affected="${REPORT_DIR}/${name}/src_affected.txt"
 
-    if [[ $(stat -c%s "${abi_affected}") > 0 ]]; then
+    if [[ $(stat -c%s "${abi_affected}") -gt 0 ]]; then
         echo
         echo "ABI breakages detected:"
         cat ${abi_affected} | c++filt
         echo "List saved to file ${abi_affected}"
     fi
-    if [[ $(stat -c%s "${src_affected}") > 0 ]]; then
+    if [[ $(stat -c%s "${src_affected}") -gt 0 ]]; then
         echo
         echo "API breakages detected:"
         cat ${src_affected} | c++filt
@@ -152,9 +152,9 @@ print_abi_breakages()
 
 compare_dumps()
 {
-    local name=$1
-    local old_dump=$2
-    local new_dump=$3
+    local name=${1}
+    local old_dump=${2}
+    local new_dump=${3}
     local output=${REPORT_DIR}/${name}/abi_dumper_compare_output.txt
 
     mkdir -p ${REPORT_DIR}/${name}
@@ -173,9 +173,9 @@ compare_dumps()
 
 check_abi()
 {
-    local name=$1
-    local old_dump=$2
-    local new_dump=$3
+    local name=${1}
+    local old_dump=${2}
+    local new_dump=${3}
     local output_dir=${REPORT_DIR}/${name}
 
     mkdir -p ${output_dir}
@@ -189,7 +189,7 @@ check_abi()
                             -strict \
                             -list-affected \
                             -report-path ${output_dir}/compat_report.html
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
         print_abi_breakages ${name}
         ST_ERROR=1
     fi
@@ -203,7 +203,7 @@ check_abi()
 run()
 {
     if [[ -z "${DUMP_DIR}" ]]; then
-        readonly DUMPS_TMP_DIR=`mktemp -d -t ABI-XXXXXXXXXX` # temp dir
+        readonly DUMPS_TMP_DIR=$(mktemp -d -t ABI-XXXXXXXXXX) # temp dir
         echo "Temp dir: ${DUMPS_TMP_DIR}"
     else
         readonly DUMPS_TMP_DIR=${DUMP_DIR}
@@ -222,7 +222,7 @@ clean()
     echo "Deleting temporary files ..."
     if [[ -z "${DUMP_DIR}" ]] && [[ ! -z "${DUMPS_TMP_DIR}" ]]; then
         echo "Delete ${DUMPS_TMP_DIR}"
-        rm -r ${DUMPS_TMP_DIR}
+        rm -r "${DUMPS_TMP_DIR}"
     fi
     echo
 }
