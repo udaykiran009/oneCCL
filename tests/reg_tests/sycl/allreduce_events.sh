d2622f2dfce60cdefdf7a49037a8f139038c4ee8
Dmitriy Sazanov <Dmitriy.Sazanov@intel.com>
2021-09-08 15:32:40 +0300

    Add launch parameters for allreduce_events (#1331)

--- tests/reg_tests/sycl/allreduce_events.sh
+++ tests/reg_tests/sycl/allreduce_events.sh
@@ -11,7 +11,7 @@ source ${ROOT_DIR}/utils.sh
 check_impi
 check_ccl
 
-mpiexec.hydra -n 2 -ppn 1 ${SCRIPT_DIR}/${BINFILE} > ${TEST_LOG} 2>&1
+CCL_KERNEL_OUTPUT_EVENT=1 EnableDirectSubmission=1 CCL_ZE_CLOSE_IPC_WA=1 mpiexec.hydra -n 2 -ppn 1 ${SCRIPT_DIR}/${BINFILE} > ${TEST_LOG} 2>&1
 rc=$?
 
 if [[ ${rc} -ne 0 ]] ; then
