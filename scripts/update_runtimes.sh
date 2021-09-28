7eeafa22f1bd862d5e980ab6867cc09567cc0e59
Dmitriy Sazanov <Dmitriy.Sazanov@intel.com>
2021-09-28 13:36:52 +0300

    Add unpack runtimes for update_runtimes.sh (#1371)
    For https://jira.devtools.intel.com/browse/INFRA-1847
    (Automate update l0 and opencl runtimes in Lab)

--- scripts/update_runtimes.sh
+++ scripts/update_runtimes.sh
@@ -1,41 +1,22 @@
 #!/bin/bash
 
-BASENAME=`basename $0 .sh`
-SCRIPT_DIR=`cd $(dirname "$BASH_SOURCE") && pwd -P`
-
-LEVEL_ZERO_PACKAGES=""
-NEO_PACKAGES=""
-
 INTEL_GRAPHICS_COMPILER_URL="https://api.github.com/repos/intel/intel-graphics-compiler/releases/latest"
 COMPUTE_RUNTIME_URL="https://api.github.com/repos/intel/compute-runtime/releases/latest"
 LEVEL_ZERO_URL="https://api.github.com/repos/oneapi-src/level-zero/releases/latest"
 
-ARTEFACT_DIR="/p/pdsd/scratch/jenkins/artefacts"
-CCL_ONEAPI_DIR="/p/pdsd/scratch/Uploads/CCL_oneAPI/"
-
 print_help()
 {
     echo "Usage:"
-    echo "    ./${BASENAME}.sh <options>"
+    echo "    ./update-runtimes.sh <options>"
     echo ""
     echo "<options>:"
     echo "   ------------------------------------------------------------"
-    echo "    -update-runtimes|--update-runtimes"
-    echo "        Download Level-Zero, NEO runtimes and install it"
-    echo "    -download-level-zero|--download-level-zero"
-    echo "        Download Level-Zero runtimes"
-    echo "    -download-neo|--download-neo"
-    echo "        Download NEO runtimes"
-    echo "    -install|--install"
-    echo "        Install runtimes"
-    echo "    -f|--runtime-dir"
-    echo "        The path to the directory where the runtime packages will be uploaded"
-    echo "    -run-examples|--run-examples"
-    echo "        Run examples to check the correct installation of runtimes"
-    echo "    -p|-password"
-    echo "        Path to file with password"
-    echo "    -ccl-src|--ccl-src"
-    echo "        Path to dir with oneCCL source files"
+    echo "    -u|--unpack"
+    echo "        Unpacking .deb packages"
+    echo "    -unpack-dir|--unpack-dir"
+    echo "        The path to the directory where the runtime packages will be unpacked"
+    echo "    -check-version|--check-version"
+    echo "        Comparison of the latest available version and the version of the system"
     echo "   ------------------------------------------------------------"
     echo "    -h|-H|-help|--help"
     echo "        Print help information"
@@ -47,38 +28,19 @@ parse_arguments()
     while [ $# -ne 0 ]
     do
         case $1 in
-            "--update-runtimes"|"-update-runtimes")
-                DOWNLOAD_L0="yes"
-                DOWNLOAD_NEO="yes"
-                ENABLE_INSTALL="yes"
-                ;;
-            "--download-level-zero"|"-download-level-zero")
-                DOWNLOAD_L0="yes"
-                ;;
-            "--download-neo"|"-download-neo")
-                DOWNLOAD_NEO="yes"
-                ;;
-            "--install"|"-install")
-                ENABLE_INSTALL="yes"
-                ;;
-            "--run-examples"|"-run-examples")
-                RUN_EXAMPLES="yes"
-                ;;
-            "--ccl-src"|"-ccl-src")
-                CCL_SRC_DIR="${2}"
-                shift
-                ;;
             "-help"|"--help"|"-h"|"-H")
                 print_help
                 exit 0
                 ;;
-            "-f"|"--runtime-dir")
-                RUNTIMES_DIR="${2}"
+            "-u"|"--unpack")
+                UNPACK="yes"
+                ;;
+            "-unpack-dir"|"--unpack-dir")
+                UNPACK_DIR="${2}"
                 shift
                 ;;
-            "-p"|"--password")
-                PASSWORD="${2}"
-                shift
+            "-check-version"|"--check-version")
+                CHECK_VERSION="yes"
                 ;;
             *)
                 echo "ERROR: unknown option ($1)"
@@ -90,19 +52,16 @@ parse_arguments()
         shift
     done
 
-    if [[ -z ${RUNTIMES_DIR} ]]; then
-        RUNTIMES_DIR="/tmp/neo"
+    if [[ -z ${UNPACK_DIR} && ! -z ${UNPACK} ]]; then
+        UNPACK_DIR="/p/pdsd/scratch/Uploads/runtimes"
     fi
 
     echo "-----------------------------------------------------------"
     echo "PARAMETERS"
     echo "-----------------------------------------------------------"
-    echo "DOWNLOAD_L0          = ${DOWNLOAD_L0}"
-    echo "DOWNLOAD_NEO         = ${DOWNLOAD_NEO}"
-    echo "ENABLE_INSTALL       = ${ENABLE_INSTALL}"
-    echo "RUNTIMES_DIR         = ${RUNTIMES_DIR}"
-    echo "CCL_SRC_DIR          = ${CCL_SRC_DIR}"
-    echo "RUN_EXAMPLES         = ${RUN_EXAMPLES}"
+    echo "UNPACK               = ${UNPACK}"
+    echo "UNPACK_DIR           = ${UNPACK_DIR}"
+    echo "CHECK_VERSION        = ${CHECK_VERSION}"
     echo "-----------------------------------------------------------"
 }
 
@@ -119,154 +78,85 @@ CheckCommandExitCode() {
     fi
 }
 
-load_level_zero() {
-    if [[ ! -z ${DOWNLOAD_L0} ]]; then
-        print_header "LOAD LEVEL ZERO..."
-        mkdir -p ${RUNTIMES_DIR}
-        cd ${RUNTIMES_DIR}
-        for package in $(curl -s ${LEVEL_ZERO_URL} | grep "browser_download_url" | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
-        do
-            echo "wget ${package}"
-            wget ${package}
-            LEVEL_ZERO_PACKAGES="${LEVEL_ZERO_PACKAGES} $(basename ${package})"
-        done
-        check_version "${LEVEL_ZERO_PACKAGES}"
-        if [[ $? -eq 0 ]]; then
-            echo "All versions are up to date"
-        fi
-    fi
-}
+unpack_packages() {
+    if [[ ! -z ${UNPACK} ]]; then
+        print_header "UNPACK PACKAGES..."
+        TAG_LEVEL_ZERO=$(curl ${LEVEL_ZERO_URL} | grep tag_name | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g')
+        TAG_COMPUTE_RUNTIME=$(curl ${COMPUTE_RUNTIME_URL} | grep tag_name | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g')
 
-check_sha_sum() {
-    print_header "CHECK SHA SUM..."
-    sum=$(curl -s ${COMPUTE_RUNTIME_URL} | grep "browser_download_url" | awk '{print $2}' | grep "sum" | sed -e 's/^"//' -e 's/"$//')
-    cd ${RUNTIMES_DIR}
-    wget ${sum}
-    sha256sum -c $(basename ${sum})
-    rm $(basename ${sum})
-}
-
-load_neo() {
-    if [[ ! -z ${DOWNLOAD_NEO} ]]; then
-        print_header "LOAD NEO..."
-        mkdir -p ${RUNTIMES_DIR}
-        cd ${RUNTIMES_DIR}
-        for package in $(curl -s ${INTEL_GRAPHICS_COMPILER_URL} | grep "browser_download_url" | grep "core\|opencl_" | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
-        do
-            wget ${package}
-            NEO_PACKAGES="${NEO_PACKAGES} $(basename ${package})"
-        done
-        for package in $(curl -s ${COMPUTE_RUNTIME_URL} | grep "browser_download_url" | awk '{print $2}' | grep "deb" | sed -e 's/^"//' -e 's/"$//')
-        do
-            wget ${package}
-            NEO_PACKAGES="${NEO_PACKAGES} $(basename ${package})"
-        done
-        check_sha_sum
-        check_version "${NEO_PACKAGES}"
-        if [[ $? -eq 0 ]]; then
-            echo "All versions are up to date"
-        fi
-    fi
-}
-
-install() {
-    if [[ ! -z ${ENABLE_INSTALL} ]]; then
-        print_header "INSTALL..."
-        chmod -R 755 ${RUNTIMES_DIR}
-        cd ${RUNTIMES_DIR}
-        for package in $(ls ${RUNTIMES_DIR})
-        do
-            /usr/bin/sudo -S dpkg -i ${package} < ${PASSWORD}
-            CheckCommandExitCode $? "Install ${package} failed"
-        done
-    fi
-}
-
-check_version() {
-    print_header "CHECK VERSION..."
-    rc=0
-    for package in ${1}
-    do  
-        pkg_name=$(echo ${package} | cut -d'_' -f1)
-        current_version=$(dpkg -l ${pkg_name} | grep ${pkg_name} | awk '{print $3}')
-        new_version=$(echo ${package} | cut -d'_' -f2 | cut -d'%' -f1)
-        if [[ ${current_version} == ${new_version} ]]
-        then
-            echo "The ${package} package has the current version."
+        print_header "UNPACK LEVEL ZERO (${TAG_LEVEL_ZERO})"
+        if [ -e ${UNPACK_DIR}/level-zero/${TAG_LEVEL_ZERO} ]; then
+            echo "This version is already unpacked into ${UNPACK_DIR}/level-zero"
         else
-            echo "The ${package} version outdated (current version: ${current_version} | new_version: ${new_version})."
-            rc=$((rc+1))
+            mkdir -p ${UNPACK_DIR}/level-zero/${TAG_LEVEL_ZERO}/{packages,unpack}
+
+            cd ${UNPACK_DIR}/level-zero/${TAG_LEVEL_ZERO}/packages
+            for package in $(curl -s ${LEVEL_ZERO_URL} | grep "browser_download_url" | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')  
+            do
+                echo "wget ${package}"
+                wget ${package}
+            done
+
+            for package in $(ls ${UNPACK_DIR}/level-zero/${TAG_LEVEL_ZERO}/packages)
+            do
+                dpkg -x ${UNPACK_DIR}/level-zero/${TAG_LEVEL_ZERO}/packages/${package} ${UNPACK_DIR}/level-zero/${TAG_LEVEL_ZERO}/unpack
+            done
+
+            rm -rf ${UNPACK_DIR}/level-zero/last
+            ln -s ${UNPACK_DIR}/level-zero/${TAG_LEVEL_ZERO} ${UNPACK_DIR}/level-zero/last
         fi
-    done
-    return ${rc}
+
+        print_header "UNPACK COMPUTE RUNTIME (${TAG_COMPUTE_RUNTIME})"
+        if [ -e ${UNPACK_DIR}/compute-runtime/${TAG_COMPUTE_RUNTIME} ]; then
+            echo "This version is already unpacked into ${UNPACK_DIR}/compute-runtime"
+        else
+            mkdir -p ${UNPACK_DIR}/compute-runtime/${TAG_COMPUTE_RUNTIME}/{packages,unpack}
+            cd ${UNPACK_DIR}/compute-runtime/${TAG_COMPUTE_RUNTIME}/packages
+            for package in $(curl -s ${COMPUTE_RUNTIME_URL} | grep "browser_download_url" | awk '{print $2}' | grep "deb" | sed -e 's/^"//' -e 's/"$//')
+            do
+                echo "wget ${package}"
+                wget ${package}
+            done
+            for package in $(curl -s ${INTEL_GRAPHICS_COMPILER_URL} | grep "browser_download_url" | grep "core\|opencl_" | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+            do
+                echo "wget ${package}"
+                wget ${package}
+            done
+
+            for package in $(ls ${UNPACK_DIR}/compute-runtime/${TAG_COMPUTE_RUNTIME}/packages)
+            do
+                dpkg -x ${UNPACK_DIR}/compute-runtime/${TAG_COMPUTE_RUNTIME}/packages/${package} ${UNPACK_DIR}/compute-runtime/${TAG_COMPUTE_RUNTIME}/unpack
+            done
+
+            rm -rf ${UNPACK_DIR}/compute-runtime/last
+            ln -s ${UNPACK_DIR}/compute-runtime/${TAG_COMPUTE_RUNTIME} ${UNPACK_DIR}/compute-runtime/last
+        fi
+    fi
 }
 
-run_examples() {
-    if [[ ! -z ${RUN_EXAMPLES} ]] && [[ ! -z ${CCL_SRC_DIR} ]]; then
-        print_header "RUN EXAMPLES..."
-        if [[ ! -d ${CCL_SRC_DIR} ]]; then
-            echo "${CCL_SRC_DIR} does not exists."
-            exit 1
-        fi
+check_versions() {
+    if [[ ! -z ${CHECK_VERSION} ]]; then
+        print_header "CHECK VERSIONS..."
+        for link in ${LEVEL_ZERO_URL} ${COMPUTE_RUNTIME_URL} ${INTEL_GRAPHICS_COMPILER_URL}
+        do
+            packages=`curl ${link} | awk '/name/ && /.deb/ && !/body/ && gsub(/"/,"") && gsub(/_/," ") && !/intel-igc-media/ && !/intel-igc-opencl-devel/ {print $2, $3}'`
+            latest_packages_list+="${packages}\n"
+        done
+        echo -e "\nLATEST_PACKAGES_LIST:\n${latest_packages_list}"
+        for package_name in `echo -e "${latest_packages_list}" | awk '!/intel-igc-media/ && !/intel-igc-opencl-devel/ {print $1}'`
+        do
+            system_packages_list+="`dpkg -l ${package_name} | grep ${package_name} | awk '{print $2,$3}'`\n"
+        done
+        echo -e "\nSYSTEM_PACKAGES_LIST:\n${system_packages_list}"
 
-        if [[ -d "${ARTEFACT_DIR}/impi-ch4-weekly/last/oneapi_impi/" ]]
-        then
-            weekly_mpiexec_version=$(${ARTEFACT_DIR}/impi-ch4-weekly/last/oneapi_impi/bin/mpiexec --version)
-            weekly_build_date=$(echo ${weekly_mpiexec_version#*Build} | awk '{print $1;}')
-
-            lkg_mpiexec_version=$(${CCL_ONEAPI_DIR}/mpi_oneapi/last/mpi/latest/bin/mpiexec --version)
-            lkg_build_date=$(echo ${lkg_mpiexec_version#*Build} | awk '{print $1;}')
-
-            if [ "$weekly_build_date" = "$lkg_build_date" ]; then
-                I_MPI_ROOT="${CCL_ONEAPI_DIR}/mpi_oneapi/last/mpi/latest/"
-            elif expr "$weekly_build_date" "<" "$lkg_build_date" >/dev/null; then
-                I_MPI_ROOT="${CCL_ONEAPI_DIR}/mpi_oneapi/last/mpi/latest/"
-            else
-                I_MPI_ROOT="${ARTEFACT_DIR}/impi-ch4-weekly/last/oneapi_impi"
-            fi
+        if [[ `echo -e "${system_packages_list}"` == `echo -e "${latest_packages_list}"` ]]; then
+            echo "All versions are up-to-date"
         else
-            I_MPI_ROOT="${CCL_ONEAPI_DIR}/mpi_oneapi/last/mpi/latest/"
+            echo "Some packages have an outdated version"
         fi
-
-        CCL_PATH="/p/pdsd/scratch/jenkins/artefacts/ccl-nightly/last/"
-        SYCL_BUNDLE_ROOT="/p/pdsd/scratch/Uploads/CCL_oneAPI/compiler/last/compiler/latest/linux/"
-
-        HOSTNAME=`hostname -s`
-        export I_MPI_HYDRA_HOST_FILE=${CCL_SRC_DIR}/tests/cfgs/clusters/${HOSTNAME}/mpi.hosts
-
-        source ${SYCL_BUNDLE_ROOT}/../../../setvars.sh
-        source ${I_MPI_ROOT}/env/vars.sh -i_mpi_library_kind=release_mt
-        source ${CCL_PATH}/l_ccl_release*/env/vars.sh --ccl-configuration=cpu_gpu_dpcpp
-
-        ${CCL_SRC_DIR}/examples/run.sh gpu
-        CheckCommandExitCode $? "Examples failed"
     fi
 }
 
 parse_arguments "$@"
-
-if [[ -d ${RUNTIMES_DIR} ]]; then
-    echo "Path ${RUNTIMES_DIR} already exists. Do you want to overwrite the data?"
-    echo "Type 'y' to continue or 'q' to exit and then press Enter"
-    CONFIRMED=0
-    while [ ${CONFIRMED} = 0 ]
-    do
-        read -e ANSWER
-        case $ANSWER in
-            'q')
-                exit 0
-                ;;
-            'y')
-                CONFIRMED=1
-                rm -rf ${RUNTIMES_DIR}
-                ;;
-            *)
-                ;;
-        esac
-    done
-fi
-
-load_level_zero
-load_neo
-install
-run_examples
+check_versions
+unpack_packages
