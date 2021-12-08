b0512a8e0e46f5b75f84b2513e195ad4966ac8ef
ksenyako <ksenya.kochanova@intel.com>
2021-12-09 00:57:55 +0300

    Fix for mlsl-1160: Incorrect buffer result on Aurora with -n 24 -ppn 12 (#1519)
    Added coefficients to avoid overflow
    or accuracy issue on large scale and with lp float data types.
    Tested manually with -n 24 on CLX03 and Aurora

--- tests/reg_tests/sycl/allreduce_bidir_algo.sh
+++ tests/reg_tests/sycl/allreduce_bidir_algo.sh
@@ -16,7 +16,7 @@ cd ${SCRIPT_DIR}
 export CCL_LOG_LEVEL=info
 export CCL_ZE_BIDIR_ALGO=1
 
-bench_options="-b sycl -w 4 -i 8 -c all -l allreduce -t 2097152"
+bench_options="-d all -b sycl -w 4 -i 8 -c all -l allreduce -t 2097152"
 
 mpiexec -l -n 2 -ppn 2 ${SCRIPT_DIR}/benchmark ${bench_options} > ${TEST_LOG} 2>&1
 ret_val=$?
