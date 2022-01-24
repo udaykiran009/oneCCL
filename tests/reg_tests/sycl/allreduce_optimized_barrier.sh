7e7894907c21312f8388f91512ac24d91b019776
Nikita Gusev <nikita.gusev@intel.com>
2022-01-24 20:59:04 +0300

    tests: enable I_MPI_THREAD_LOCK_LEVEL=vci for fast barrier reg test (#1604)

--- tests/reg_tests/sycl/allreduce_optimized_barrier.sh
+++ tests/reg_tests/sycl/allreduce_optimized_barrier.sh
@@ -21,20 +21,21 @@ export CCL_BARRIER=direct
 
 export I_MPI_FABRICS=shm
 
-# TODO: remove after fix IMPI-3127
-export I_MPI_THREAD_LOCK_LEVEL=global
-
 bench_options="-b sycl -w 4 -i 8 -c all -l allreduce -y 1000"
+lock_levels="global vci"
 
-mpiexec -l -n 2 -ppn 2 ${SCRIPT_DIR}/benchmark ${bench_options} > ${TEST_LOG} 2>&1
-ret_val=$?
-if [ $ret_val -ne 0 ]
-then
-    echo "Fail"
-    exit -1
-fi
-
-check_log ${TEST_LOG}
+for lock_level in ${lock_levels}
+do
+    export I_MPI_THREAD_LOCK_LEVEL=${lock_level}
+    mpiexec -l -n 2 -ppn 2 ${SCRIPT_DIR}/benchmark ${bench_options} > ${TEST_LOG} 2>&1
+    ret_val=$?
+    if [ $ret_val -ne 0 ]
+    then
+        echo "Fail"
+        exit -1
+    fi
+    check_log ${TEST_LOG}
+done
 
 rm ${TEST_LOG}
 echo "Pass"
