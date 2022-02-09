a37cde2bcd81a3ee362ca2259b810b5abfc70352
Mikhail Shiryaev <mikhail.shiryaev@intel.com>
2022-02-09 23:21:56 +0300

    tests: add regress test for many allreduce iterations (#1641)
    Test executes many iterations, each iteration contains
    multiple allreduce operations with message sizes from RN50.

---
+++ tests/reg_tests/cpu/many_iterations.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd -P)
+ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
+BASENAME="$(basename $0 .sh)"
+BINFILE="${BASENAME}"
+TEST_LOG="${BASENAME}.log"
+
+source ${ROOT_DIR}/utils.sh
+
+check_impi
+check_ccl
+get_bench ${SCRIPT_DIR} ${TEST_LOG}
+
+cd ${SCRIPT_DIR}
+
+ppns="4"
+transports="mpi ofi"
+
+for ppn in ${ppns}
+do
+    for transport in ${transports}
+    do
+        cmd="CCL_ATL_TRANSPORT=${transport}"
+        cmd+=" CCL_WORKER_COUNT=2"
+        cmd+=" CCL_WORKER_AFFINITY=5-12"
+        cmd+=" I_MPI_PIN_PROCESSOR_LIST=1,2,3,4"
+        cmd+=" mpiexec -l -n $((${ppn}*2)) -ppn ${ppn}"
+        cmd+=" ${SCRIPT_DIR}/${BINFILE} > ${TEST_LOG} 2>&1"
+        run_cmd "${cmd}"
+        check_log ${TEST_LOG}
+    done
+done
+
+rm ${BINFILE} ${TEST_LOG}
+
+echo "Pass"
