cf87974f2fdcf2588307b58122be67e908517522
Mikhail Shiryaev <mikhail.shiryaev@intel.com>
2022-03-13 00:00:24 +0300

    tests: extend implicit scaling reg test (#1717)

--- tests/reg_tests/sycl/implicit_scaling.sh
+++ tests/reg_tests/sycl/implicit_scaling.sh
@@ -9,17 +9,13 @@ source ${ROOT_DIR}/utils.sh
 
 make_common_actions ${SCRIPT_DIR} ${TEST_LOG} "sycl"
 
-export SYCL_DEVICE_FILTER=level_zero
-
 transports="mpi"
 proc_counts="1 2"
 root_device_modes="0 1"
+implicit_scaling_modes="0 1"
+algos="ring topo"
 
-# TODO: MLSL-1280
-# algos="ring topo"
-algos="ring"
-
-bench_options="-w 1 -i 4 -l allreduce,bcast -c all -b sycl -t 131072 $(get_default_bench_dtype)"
+bench_options="-w 1 -i 4 -l allreduce,bcast -c all -b sycl -t 1048576 $(get_default_bench_dtype)"
 
 for transport in ${transports}
 do
@@ -27,17 +23,26 @@ do
     do
         for root_device_mode in ${root_device_modes}
         do
-            for algo in ${algos}
+            for implicit_scaling_mode in ${implicit_scaling_modes}
             do
-                cmd="CCL_ATL_TRANSPORT=${transport}"
-                cmd+=" CCL_LOG_LEVEL=info"
-                cmd+=" CCL_ALLREDUCE=${algo}"
-                cmd+=" CCL_BCAST=${algo}"
-                cmd+=" EnableImplicitScaling=1"
-                cmd+=" mpiexec -l -n ${proc_count} -ppn 4 ${SCRIPT_DIR}/benchmark"
-                cmd+=" ${bench_options} -g ${root_device_mode} > ${TEST_LOG} 2>&1"
-                run_cmd "${cmd}"
-                check_log ${TEST_LOG}
+                for algo in ${algos}
+                do
+                    if [[ "${root_device_mode}" == "0" ]] && [[ "${implicit_scaling_mode}" == "1" ]]
+                    then
+                        continue
+                    fi
+
+                    cmd="CCL_ATL_TRANSPORT=${transport}"
+                    cmd+=" CCL_LOG_LEVEL=info"
+                    cmd+=" CCL_ALLREDUCE=${algo}"
+                    cmd+=" CCL_BCAST=${algo}"
+                    cmd+=" SYCL_DEVICE_FILTER=level_zero"
+                    cmd+=" EnableImplicitScaling=${implicit_scaling_mode}"
+                    cmd+=" mpiexec -l -n ${proc_count} -ppn 4 ${SCRIPT_DIR}/benchmark"
+                    cmd+=" ${bench_options} -g ${root_device_mode} > ${TEST_LOG} 2>&1"
+                    run_cmd "${cmd}"
+                    check_log ${TEST_LOG}
+                done
             done
         done
     done
